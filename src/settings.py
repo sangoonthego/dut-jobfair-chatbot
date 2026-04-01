8b14e736449cda90438d2f24b97c1993db98ebc2
sangoonthego <knkidngoc@gmail.com>
2026-04-01 13:59:54 +0700

    config embedding and rag model

--- src/settings.py
+++ src/settings.py
@@ -1,25 +1,21 @@
 import os
-
 from llama_index.core import Settings
 from llama_index.llms.ollama import Ollama
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
-from llama_index.embeddings.openai import OpenAIEmbedding
-from llama_index.llms.openai import OpenAI
-
 
 def init_settings():
-    # if os.getenv("OPENAI_API_KEY") is None:
-    #     raise RuntimeError("OPENAI_API_KEY is missing in environment variables")
-    # Settings.llm = OpenAI(model=os.getenv("MODEL") or "gpt-4.1")
-    # Settings.embed_model = OpenAIEmbedding(
-    #     model=os.getenv("EMBEDDING_MODEL") or "text-embedding-3-large"
-    # )
-
+    # Chỉ dùng HF_HOME để nó tự tìm vào thư mục hub
+    os.environ["HF_HOME"] = r"D:\huggingface_cache"
+    
     Settings.llm = Ollama(
-        model="qwen2.5", 
-        request_timeout=360.0
+        model="qwen2.5:1.5b", 
+        request_timeout=300.0
     )
     
+    # Ở đây CHỈ để model_name, KHÔNG để cache_folder nữa để nó dùng HF_HOME
     Settings.embed_model = HuggingFaceEmbedding(
         model_name="BAAI/bge-m3"
     )
+
+    Settings.chunk_size = 512
+    Settings.chunk_overlap = 50
